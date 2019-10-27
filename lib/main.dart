c32cdff812f02fe57ac0ef0963d447ffff3acd95
YuSing <hkforgame@gmail.com>
2019-10-28 02:19:56 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:xpenz/add_event_page.dart';
 import 'package:xpenz/callback.dart';
+import 'package:xpenz/daily_notification.dart';
 import 'package:xpenz/item.dart';
 import 'my_drawer.dart';
 import 'my_icons_icons.dart';
 import 'my_appbar.dart';
+import 'daily_notification.dart';
 
 List<Item> eventItems = [];
 
-void main() {
+void main() async {
   addDrawerItem(iconData: MyIcons.wallet, title: 'Wallet');
+  await scheduleDailyNotification();
   runApp(MyApp());
 }
 
@@ -51,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
   _MyHomePageState() { callback = setState; }
   @override
   Widget build(BuildContext context) {
+    _context = context;
     return Scaffold(
       drawer: MyDrawer(),
       appBar: AppBar(title: MyAppBar()),
